db6c78241e8ff3afdd7e94ccf792e1f49731df92
leonardovalle34 <leonardovalle18@gmail.com>
2023-08-27 18:10:19 -0300

    paginaçao

--- src/Pages/Maincard.tsx
+++ src/Pages/Maincard.tsx
@@ -4,19 +4,83 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 export default function MainCard() {
-  const [pokemons, setPokemons] = useState<Array>([]);
+  const [pokemons, setPokemons] = useState<any>([]);
+  const [firstEndPoint, setFirstEndPoint] = useState<any>([]);
+  const [nextEndPoint, setNextEndPoint] = useState<any>([]);
+  const [previousEndPoint, setPreviousEndPoint] = useState<any>("");
+  const [totalPages, setTotalPages] = useState<any>();
+  const [lastPoke, setLastPoke] = useState<any>();
+  const [pages, setPages] = useState<any>();
+  const [actualPage, setActualPage] = useState<any>();
 
-  const load = async () => {
-    const endpoints = [];
-    for (let x: number = 1; x <= 12; x++) {
-      endpoints.push(`https://pokeapi.co/api/v2/pokemon/${x}`);
+  const pagination = () => {
+    console.log(lastPoke);
+    if (totalPages > 0) {
+      setPages(totalPages / 20);
+      setActualPage(lastPoke / 20);
     }
-    axios
-      .all(endpoints.map((endpoint) => axios.get(endpoint)))
-      .then((res) => setPokemons(res));
   };
 
-  console.log(pokemons);
+  const getData = async (newEndPoints: Array) => {
+    setLastPoke(
+      Number(newEndPoints?.data?.result[res?.data?.result.length - 1])
+    );
+    axios
+      .all(newEndPoints.map((endPoint: any) => axios.get(endPoint)))
+      .then((res: any) => setPokemons(res));
+
+    /*axios.get(`${endPoint?.data?.next}`).then((res) => {
+      setEndPoint(res);
+    });*/
+    pagination();
+  };
+
+  const getEndPoints = async (firstparam) => {
+    const endPoints = [firstparam];
+    const paramEndPoints: any = [];
+
+    if (endPoints) {
+      endPoints[0]?.map((res: any) => {
+        paramEndPoints.push(res.url);
+      });
+      getData(paramEndPoints);
+    }
+  };
+
+  const load = async () => {
+    await axios
+      .get("https://pokeapi.co/api/v2/pokemon?offset=0&limit=20")
+      .then((res: any) => {
+        //setLastPoke(res.data.results[0].url);
+        setFirstEndPoint(res?.data?.results);
+        getEndPoints(res?.data?.results);
+        setNextEndPoint(res?.data?.next);
+        setTotalPages(res?.data?.count);
+        setLastPoke(res?.data?.results[res?.data?.results?.length - 1].url);
+      });
+  };
+
+  const pageUp = async () => {
+    await axios.get(`${nextEndPoint}`).then((res: any) => {
+      //setLastPoke(res?.data?.result[0].url);
+      getEndPoints(res?.data?.results);
+      setNextEndPoint(res.data.next);
+      setPreviousEndPoint(res.data.previous);
+      setTotalPages(res?.data?.count);
+      setLastPoke(res?.data?.results[res?.data?.results?.length - 1].url);
+    });
+  };
+
+  const pageDown = async () => {
+    await axios.get(`${previousEndPoint}`).then((res: any) => {
+      //setLastPoke(res?.data?.result[0].url);
+      getEndPoints(res?.data?.results);
+      setNextEndPoint(res.data.next);
+      setPreviousEndPoint(res.data.previous);
+      setTotalPages(res?.data?.count);
+      setLastPoke(res?.data?.results[res?.data?.results?.length - 1].url);
+    });
+  };
 
   useEffect(() => {
     load();
@@ -24,6 +88,16 @@ export default function MainCard() {
 
   return (
     <>
+      <button onClick={() => pageDown()}>Previous</button>
+      <button onClick={() => pageUp()}>Next</button>
+      <p>
+        Pagina{" "}
+        <strong>
+          {actualPage}
+          <strong> de </strong>
+          {pages}
+        </strong>
+      </p>
       <MainCardStyled>
         {pokemons?.map((el: any, i: number) => {
           return (
